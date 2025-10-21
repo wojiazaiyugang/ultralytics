47df79508350f976c71ead9083077306c9edecd4
yujiannan <wojiazaiyugang@outlook.com>
2025-10-21 18:14:10 +0800

    人体检测实验2配置

--- scripts/train_human_detection.py
+++ scripts/train_human_detection.py
@@ -8,9 +8,10 @@ model = YOLO("yolo11m.pt")
 # Train the model
 results = model.train(data=r"C:\Users\yujiannan\Downloads\Classroom mointoring.v1i.yolov11\data.yaml",
                       epochs=300,
-                      imgsz=640,
+                      batch=0.9,
+                      imgsz=640 * 2,
                       project="human_detection",
-                      name="1",
+                      name="2",
                       degrees=10,
                       shear=10,
                       perspective=0.0005,
