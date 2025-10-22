0e158de7daba7ad159c243e866a340e0032c9aff
yujiannan <wojiazaiyugang@outlook.com>
2025-10-22 15:23:47 +0800

    使用comet_ml记录实验，实验2重新开始训练

--- scripts/train_human_detection.py
+++ scripts/train_human_detection.py
@@ -12,9 +12,11 @@ model = YOLO("yolo11m.pt")
 
 results = model.train(data=r"D:\Datasets\process_20251013_20251014_20251018_20251023\data.yaml",
                       epochs=300,
-                      imgsz=640,
+                      imgsz=640*2,
+                      batch=8,
+                      exist_ok=True,
                       project="human_detection",
-                      name="1",
+                      name="2",
                       degrees=10,
                       shear=10,
                       perspective=0.0005,
