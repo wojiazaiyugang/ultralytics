6dde148e1cdcce7d07b52f52c0c7e800617eea0b
yujiannan <wojiazaiyugang@outlook.com>
2025-10-23 16:20:28 +0800

    实验6 实验4+加上20251013_20251014_20251018_20251023

--- scripts/train_human_detection.py
+++ scripts/train_human_detection.py
@@ -10,14 +10,15 @@ comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
 
 model = YOLO("yolo11m.pt")
 
-results = model.train(data=r"D:\Datasets\process_20251022\data.yaml",
+results = model.train(data=r"D:\Datasets\process_20251013_20251014_20251018_20251022_20251023\data.yaml",
                       epochs=300,
                       imgsz=640*2,
-                      batch=8,
+                      batch=12,
                       exist_ok=True,
                       project="human_detection",
-                      name="5",
+                      name="6",
                       degrees=10,
                       shear=10,
                       perspective=0.0005,
+                      mixup=0.1,
                       cutmix=0.1)
