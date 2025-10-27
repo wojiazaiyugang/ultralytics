4cc51671fcbe6a0fbc569067eec1d3ceadb43477
yujiannan <wojiazaiyugang@outlook.com>
2025-10-27 15:55:31 +0800

    座椅检测实验1

--- scripts/train_chair_detection.py
+++ scripts/train_chair_detection.py
@@ -8,18 +8,15 @@ comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
                workspace="wojiazaiyugang",
                project_name="chair-detection")
 
-model = YOLO(r"D:\Projects\ultralytics\scripts\human_detection\4\weights\best.pt")
+model = YOLO("yolo11m.pt")
 
-results = model.train(data=r"D:\Datasets\process_20251013_20251014_20251018_20251023\data.yaml",
-                      lr0=0.001,
-                      lrf=0.001,
-                      freeze=10,
-                      epochs=50,
+results = model.train(data=r"D:/Datasets/process_20251025_20251026/data.yaml",
+                      epochs=300,
                       imgsz=640*2,
                       batch=12,
                       exist_ok=True,
-                      project="human_detection",
-                      name="7",
+                      project="chair_detection",
+                      name="1",
                       degrees=10,
                       shear=10,
                       perspective=0.0005,
