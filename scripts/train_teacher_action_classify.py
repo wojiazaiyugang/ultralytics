1296f13b32b7e7432fea9513b179c66cd853bd4f
yujiannan <wojiazaiyugang@outlook.com>
2025-11-13 18:20:31 +0800

    老师动作分类实验1

---
+++ scripts/train_teacher_action_classify.py
@@ -0,0 +1,17 @@
+import comet_ml
+from ultralytics import YOLO
+
+
+comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
+               workspace="wojiazaiyugang",
+               project_name="teacher-action-classify")
+
+model = YOLO("yolo11m-cls.pt")  # load a pretrained model (recommended for training)
+
+# Train the model
+results = model.train(data="/DATA/yujiannan/Datasets/20251113",
+                      epochs=300,
+                      imgsz=224,
+                      exist_ok=True,
+                      project="teacher_action_classify",
+                      name="1")
