df55d2dff9e984bea5016e471df2976990c72f96
yujiannan <wojiazaiyugang@outlook.com>
2025-11-28 10:35:31 +0800

    老师动作分类实验2 20251128数据集

--- scripts/train_teacher_action_classify.py
+++ scripts/train_teacher_action_classify.py
@@ -9,9 +9,9 @@ comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
 model = YOLO("yolo11m-cls.pt")  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data="/DATA/yujiannan/Datasets/20251113",
+results = model.train(data="/DATA/yujiannan/Datasets/20251128",
                       epochs=300,
                       imgsz=224,
                       exist_ok=True,
                       project="teacher_action_classify",
-                      name="1")
+                      name="2")
