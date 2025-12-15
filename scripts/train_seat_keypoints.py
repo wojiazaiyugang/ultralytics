d09081bb7a63fe975c45fb1362eb142f0efa377b
yujiannan <wojiazaiyugang@outlook.com>
2025-12-15 16:59:34 +0800

    座椅关键点检测实验3

--- scripts/train_seat_keypoints.py
+++ scripts/train_seat_keypoints.py
@@ -10,13 +10,13 @@ comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
 
 model = YOLO("yolo11m-pose.pt")
 
-results = model.train(data=r"/DATA/yujiannan/Datasets/座椅关键点/data.yaml",
+results = model.train(data=r"/DATA/yujiannan/Datasets/process_20251215/data.yaml",
                       epochs=300,
                       imgsz=640*2,
                       batch=12,
                       exist_ok=True,
                       project="seat_keypoints",
-                      name="2",
+                      name="3",
                       degrees=10,
                       shear=10,
                       perspective=0.0005,
