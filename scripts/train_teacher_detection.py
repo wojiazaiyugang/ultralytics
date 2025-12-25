0d37c53404952445d3aacd8e440a8c7f60902f66
yujiannan <wojiazaiyugang@outlook.com>
2025-12-25 15:09:53 +0800

    老师检测实验3

--- scripts/train_teacher_detection.py
+++ scripts/train_teacher_detection.py
@@ -8,15 +8,15 @@ comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
                workspace="wojiazaiyugang",
                project_name="teacher-detection")
 
-model = YOLO(r"yolo11m.pt")
+model = YOLO(r"yolo11s.pt")
 
 results = model.train(data=r"/DATA/yujiannan/Datasets/process_20251209_20251210/data.yml",
                       epochs=300,
-                      imgsz=640*2,
-                      batch=12,
+                      imgsz=640,
+                      batch=24,
                       exist_ok=True,
                       project="logs/teacher_detection",
-                      name="2",
+                      name="3",
                       degrees=10,
                       shear=10,
                       perspective=0.0005,
