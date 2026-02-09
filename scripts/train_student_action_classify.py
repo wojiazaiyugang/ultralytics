41adb16c8a5621344089f6d63728c77a4547e6e1
yujiannan <wojiazaiyugang@outlook.com>
2026-02-09 13:34:04 +0800

    学生动作分类实验5

--- scripts/train_student_action_classify.py
+++ scripts/train_student_action_classify.py
@@ -6,13 +6,12 @@ comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
                workspace="wojiazaiyugang",
                project_name="student-action-classify")
 
-model = YOLO("yolo11m-cls.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo11s-cls.pt")  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data=r"/DATA/yujiannan/Datasets/20251121",
+results = model.train(data=r"/DATA/yujiannan/Datasets/20260210",
                       epochs=300,
                       imgsz=224,
                       exist_ok=True,
                       project="logs/student_action_classify",
-                      auto_augment="augmix",
-                      name="4")
+                      name="5")
