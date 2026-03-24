c485cc716c4eaccc60bc4c932eefcbe9d7841dfc
yujiannan <wojiazaiyugang@outlook.com>
2026-03-24 14:36:47 +0800

    学生动作分类实验7 重新实现一下实验6，因为实验6有pickle问题

--- scripts/train_student_action_classify.py
+++ scripts/train_student_action_classify.py
@@ -1,37 +1,56 @@
-import comet_ml
-from ultralytics import YOLO
-from ultralytics.data import augment
-from ultralytics.data.augment import ClassifyLetterBox, ToTensor, DEFAULT_MEAN, DEFAULT_STD, classify_augmentations
-from ultralytics.models.yolo.classify import ClassificationTrainer
-from ultralytics.data.dataset import ClassificationDataset
 from typing import Any
-import torchvision.transforms as T
-import torch
+
+import comet_ml
+import cv2
 import numpy as np
+from PIL import Image
+from torchvision.transforms import functional as F
 
-
-from ultralytics.data.augment import ClassifyLetterBox, ToTensor, DEFAULT_MEAN, DEFAULT_STD
-
-from PIL import Image, ImageOps
+from ultralytics import YOLO
+from ultralytics.data.dataset import ClassificationDataset
+from ultralytics.models.yolo.classify import ClassificationTrainer
 
 comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
                workspace="wojiazaiyugang",
                project_name="student-action-classify")
-class SquarePadToMaxSide:
 
-    def __call__(self, img: Image.Image) -> Image.Image:
-        w, h = img.size
-        s = max(w, h)
-        pad_w = s - w
-        pad_h = s - h
-        padding = (pad_w // 2, pad_h // 2, pad_w - pad_w // 2, pad_h - pad_h // 2)
-        return ImageOps.expand(img, border=padding, fill=(114, 114, 114))
-
+def pad_to_square(img: Image.Image, fill=114):
+    w, h = img.size
+    max_side = max(w, h)
+    pad_w = max_side - w
+    pad_h = max_side - h
+    padding = (pad_w // 2, pad_h // 2, pad_w - pad_w // 2, pad_h - pad_h // 2)
+    return F.pad(img, padding, fill=fill)
 
 class Dataset(ClassificationDataset):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.torch_transforms = T.Compose([SquarePadToMaxSide(), self.torch_transforms])
+
+    def __getitem__(self, i: int) -> dict:
+        """
+        Return subset of data and targets corresponding to given indices.
+
+        Args:
+            i (int): Index of the sample to retrieve.
+
+        Returns:
+            (dict): Dictionary containing the image and its class index.
+        """
+        f, j, fn, im = self.samples[i]  # filename, index, filename.with_suffix('.npy'), image
+        if self.cache_ram:
+            if im is None:  # Warning: two separate if statements required here, do not combine this with previous line
+                im = self.samples[i][3] = cv2.imread(f)
+        elif self.cache_disk:
+            if not fn.exists():  # load npy
+                np.save(fn.as_posix(), cv2.imread(f), allow_pickle=False)
+            im = np.load(fn)
+        else:  # read image
+            im = cv2.imread(f)  # BGR
+        # Convert NumPy array to PIL image
+        im = Image.fromarray(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+        im = pad_to_square(im) # <<<<<
+        sample = self.torch_transforms(im)
+        return {"img": sample, "cls": j}
 
 
 class Trainer(ClassificationTrainer):
@@ -48,4 +67,4 @@ results = model.train(trainer=Trainer,
                       imgsz=224,
                       exist_ok=True,
                       project="logs/student_action_classify",
-                      name="6")
+                      name="7")
