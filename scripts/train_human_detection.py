322cca8562e5bfe6bc1f29227a62776dbc8e9cae
yujiannan <wojiazaiyugang@outlook.com>
2026-03-30 18:31:35 +0800

    人体检测实验11 从头训练

--- scripts/train_human_detection.py
+++ scripts/train_human_detection.py
@@ -8,18 +8,18 @@ comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
                workspace="wojiazaiyugang",
                project_name="human-detection")
 
-model = YOLO(r"/home/yujiannan/Projects/ultralytics/scripts/logs/human_detection/4/weights/best.pt")
+model = YOLO(r"yolo11m.pt")
 
 results = model.train(data=r"/DATA/yujiannan/Datasets/process_20251013_20260330/data.yaml",
-                      lr0=0.001,
-                      lrf=0.001,
-                      freeze=10,
-                      epochs=50,
+                      # lr0=0.001,
+                      # lrf=0.001,
+                      # freeze=10,
+                      epochs=300,
                       imgsz=640*2,
                       batch=12,
                       exist_ok=True,
                       project="logs/human_detection",
-                      name="10",
+                      name="11",
                       degrees=10,
                       shear=10,
                       perspective=0.0005)
