9946c35133362a579bb099da175e07b3dc93d1d7
yujiannan <wojiazaiyugang@outlook.com>
2026-05-20 14:48:36 +0800

    学生区域检测实验4

--- scripts/train_seat_keypoints.py
+++ scripts/train_seat_keypoints.py
@@ -10,13 +10,13 @@ comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
 
 model = YOLO("yolo11m-pose.pt")
 
-results = model.train(data=r"/DATA/yujiannan/Datasets/process_20251215/data.yaml",
+results = model.train(data=r"/DATA/yujiannan/Datasets/process_20251215_updating/data.yaml",
                       epochs=300,
                       imgsz=640*2,
                       batch=12,
                       exist_ok=True,
                       project="logs/seat_keypoints",
-                      name="3",
+                      name="4",
                       degrees=10,
                       shear=10,
                       perspective=0.0005,
