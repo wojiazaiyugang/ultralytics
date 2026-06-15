a99ec9ffe6343b24e85f7add778b06279c1185ca
yujiannan <wojiazaiyugang@outlook.com>
2026-06-15 18:47:22 +0800

    黑板检测实验4

--- scripts/train_blackboard_detection.py
+++ scripts/train_blackboard_detection.py
@@ -6,15 +6,15 @@ comet_ml.login(api_key="gq76e4j6CHnkcgarANUr5uXjV",
                workspace="wojiazaiyugang",
                project_name="blackboard-detection")
 
-model = YOLO("yolo11m.pt")
+model = YOLO("/home/yujiannan/Projects/ultralytics/scripts/logs/blackboard_detection/1/weights/best.pt")
 
 results = model.train(data=r"/DATA/yujiannan/Datasets/process_20251117_20260615_updating/data.yaml",
                       epochs=300,
                       imgsz=640*2,
-                      batch=8,
+                      batch=6,
                       exist_ok=True,
                       project="logs/blackboard_detection",
-                      name="3",
+                      name="4",
                       degrees=10,
                       shear=10,
                       perspective=0.0005,
